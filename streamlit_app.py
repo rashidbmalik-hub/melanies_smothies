02c18af24492f8f4dfddd2618bd19d8a75dc20e0
rashidbmalik-hub <rashid.b.malik@kipi.ai>
2026-05-05 16:05:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,10 @@ if ingredients_list:
 
 
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+  ingredients_string = ''
+
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ''
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
